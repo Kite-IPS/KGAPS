c4f27127abf0c5188ff114b5d77911966e965694
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2024-10-22 11:30:14 +0530

    login ui :)

--- KG-APS/src/pages/LoginComponent/Login.jsx
+++ KG-APS/src/pages/LoginComponent/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from '../../assets/image.jpg'
+import './Login.css'
 
 export default function Login() {
     return (
@@ -11,6 +12,15 @@ export default function Login() {
                         <img src={Image} alt="image" />
                     </div>
                     <div className="login-form-container">
+                        <div className="drop-down-container">
+                            <select name="roles" id="roles">
+                                <option value="faculty">Faculty</option>
+                                <option value="course-doordinator">Course Coordinator</option>
+                                <option value="domain-mentor">Domain Mentor</option>
+                                <option value="hod">H.O.D</option>
+                                <option value="admin">Admin</option>
+                            </select>
+                        </div>
                         <form method="post">
                             <input type="text" name="username" id="username" placeholder="Enter username ... " />
                             <input type="password" name="password" id="password" placeholder='Enter Password ...' />
