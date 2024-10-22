e3c0ca6690f58e88dc48a16aec32cd3ebecc1f74
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2024-10-22 11:45:29 +0530

    for mitun :)

--- KG-APS/src/App.jsx
+++ KG-APS/src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes , Route} from "react-router-dom"
 import Login from "./pages/loginComponent/Login"
-
+import Faculty from "./pages/FacultyComponent/Faculty"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>} />
+        <Route path="/" element={<Login/>} />,
+        <Route path="/faculty-incharge" element={<Faculty/>}/>
       </Routes>
     </BrowserRouter>
   )
