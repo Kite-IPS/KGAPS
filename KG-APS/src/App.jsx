cbc070bde93f6425781188ef1e3a7920392607da
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2024-10-22 12:05:31 +0530

    for pull the commit :)

--- KG-APS/src/App.jsx
+++ KG-APS/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes , Route} from "react-router-dom"
 import Login from "./pages/loginComponent/Login"
 import Faculty from "./pages/FacultyComponent/Faculty"
+import Admin from "./pages/AdminComponent/Admin"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>} />,
-        <Route path="/faculty-incharge" element={<Faculty/>}/>
+        <Route path="/" element={ <Login/> } />
+        <Route path="/faculty-incharge" element={ <Faculty/> } />
+        <Route path='/admin' element={ <Admin/> } />
       </Routes>
     </BrowserRouter>
   )
---
+++ KG-APS/src/pages/AdminComponent/app2/app2.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function app2() {
+  return (
+    <div>Hello app2</div>
+  )
+}
