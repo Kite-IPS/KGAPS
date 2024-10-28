5cf5e63b38eaf9f3b76b8aff6947181c785c4e9e
Mitun <mitun.7557@gmail.com>
2024-10-28 15:43:12 +0530

    Just in case

--- KG-APS/src/routing.jsx
+++ KG-APS/src/routing.jsx
@@ -4,6 +4,7 @@ import PieChart from "./ProgressDisplay/chart";
 import HandlingSidebar from "./pages/Handling/HandlingSidebar/HandlingSidebar";
 import HandlingFacultyDashboard from "./pages/Handling/HandlingFacultyDashboard/HandlingFacultyDashboard";
 import TestAxiosRequest from "./ProgressDisplay/testaxios";
+import HandlingTables from "./pages/Handling/HandlingTables/HandlingTables"
 
 function Routing() {
   return (
@@ -11,6 +12,7 @@ function Routing() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/handlingsidebar" element={ < HandlingSidebar />} />
+        <Route path="/handlingtables" element={ < HandlingTables />} />
         <Route path="/handlingfacultydashboard" element={ < HandlingFacultyDashboard />} />
         <Route path="/chart" element={<PieChart />} />
         <Route path="/test" element={<TestAxiosRequest />} />
