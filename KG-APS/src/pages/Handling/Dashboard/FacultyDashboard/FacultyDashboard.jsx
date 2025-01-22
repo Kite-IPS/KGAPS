4a946cb898343086f70fed12141d529b22d66c4c
Mitun <mitun.7557@gmail.com>
2025-01-22 15:39:19 +0530

    HOD & FDash style

--- KG-APS/src/pages/Handling/Dashboard/FacultyDashboard/FacultyDashboard.jsx
+++ KG-APS/src/pages/Handling/Dashboard/FacultyDashboard/FacultyDashboard.jsx
@@ -1,24 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './FacultyDashboard.css';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 import HandlingSidebar from "../../HandlingSidebar/HandlingSidebar.jsx";
 
 function HandlingFacultyDashboard() {
+  const data = JSON.parse(sessionStorage.getItem('userData'));
+  const [facultyDetails, setFacultyDetails] = useState(data); 
+  const [courseDataCurrent, setCourseDataCurrent] = useState([]);
+  const [courseDataOverall, setCourseDataOverall] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const value = (current, total) => {
     if (total === 0 || current === 0 || current > total) {
       return 100;
     }
     return (current / total) * 100;
-  }
-
-  const data = JSON.parse(sessionStorage.getItem('userData'));
-
-  const [facultyDetails, setFacultyDetails] = useState(data); 
-  const [courseDataCurrent, setCourseDataCurrent] = useState([]);
-
-  const [courseDataOverall, setCourseDataOverall] = useState([]);
+  };
 
   const renderColorComment = (barColor) => {
     switch (barColor) {
@@ -57,6 +55,7 @@ function HandlingFacultyDashboard() {
 
     fetchData();
   }, []);
+
   const aggregateData = () => {
     let aggrdata = [];
     var count = 0;
@@ -98,21 +97,20 @@ function HandlingFacultyDashboard() {
     2: "2nd Year",
     3: "3rd Year",
     4: "4th Year",
-    };
-  
+  };
+
   const sectionMap = {
     1: "A",
     2: "B"
   };
 
-  const convertToClass = (item) => {
-    const class_id = item.toString();
-    return yearMap[class_id[1]]+" - "+departmentMap[class_id[0]]+" "+sectionMap[class_id[2]];
-  };
+  const convertToClass1 = (item) => yearMap[item.toString()[1]];
+  const convertToClass2 = (item) => departmentMap[item.toString()[0]];
+  const convertToClass3 = (item) => sectionMap[item.toString()[2]];
 
   return (
     <>
-      <HandlingSidebar />
+      <HandlingSidebar /> 
       <div className="handlingfaculty-dashboard-container">
         <div className="handlingfaculty-dashboard-content">
           <div className="handlingfaculty-dashboard-nametext">
@@ -120,42 +118,56 @@ function HandlingFacultyDashboard() {
               <p className="handlingfaculty-dashboard-greeting">Welcome Faculty - {facultyDetails.name}</p>
             </div>
           </div>
-          {courseDataOverall.length>0?(
-        <>  
-        
-        <div className="handlingfaculty-dashboard-aggregate">
-          <p>Aggregate Progress</p>
-          <div className="handlingfaculty-dashboard-aggregate-content">
-          <p>Overall Progress: {(aggregateData()[0].topic_count*100).toFixed(0)}%</p>
-          <div className="handlingfaculty-dashboard-progressbar-horizontal">
-                  <div style={{ width: `${(aggregateData()[0].topic_count*100).toFixed(2)}%`, backgroundColor: 'purple' }} />
-          </div>
-            <p>Average Status: {(aggregateData()[0].comment)}</p>
-
-          </div>
-        </div>  
-        <div className="handlingfaculty-dashboard-card-container">
-          {courseDataCurrent.map((item, i) => (
-            <div className="handlingfaculty-dashboard-card" key={i}>
-              <div className="handlingfaculty-dashboard-card-header">Course: {courseDataCurrent[i].course_code+" - "+courseDataCurrent[i].course_name+" - "+convertToClass(courseDataCurrent[i].class_id)}</div>
-              <div className="handlingfaculty-dashboard-card-content">
-                <p>Hours Completed: {item.completed_hours} / {item.total_hours}</p>
-                <div className="handlingfaculty-dashboard-progressbar-horizontal">
-                  <div style={{ width: `${value(item.completed_hours,item.total_hours).toFixed(2)}%`, backgroundColor: item.bar_color }} />
-                </div>
-                <p>Topics Completed: {courseDataOverall[i].count}/{courseDataOverall[i].total_count}</p>
-                <div className="handlingfaculty-dashboard-progressbar-horizontal">
-                  <div style={{ width: `${((courseDataOverall[i].count/courseDataOverall[i].total_count)*100).toFixed(2)}%`, backgroundColor: 'green' }} />
-                </div>
-                <div className="handlingfaculty-dashboard-colorcomment">
-                  {renderColorComment(item.bar_color)}
+          {loading ? (
+            <h1>Loading...</h1>
+          ) : error ? (
+            <h1>{error}</h1>
+          ) : courseDataOverall.length > 0 ? (
+            <>
+              <div className="handlingfaculty-dashboard-aggregate">
+                <p>Aggregate Progress</p>
+                <div className="handlingfaculty-dashboard-aggregate-content">
+                  <p>Overall Progress: {(aggregateData()[0].topic_count * 100).toFixed(0)}%</p>
+                  <div className="handlingfaculty-dashboard-progressbar-horizontal">
+                    <div style={{ width: `${(aggregateData()[0].topic_count * 100).toFixed(2)}%`, backgroundColor: 'purple' }} />
+                  </div>
+                  <p>Average Status: {aggregateData()[0].comment}</p>
                 </div>
               </div>
-            </div>
-          ))}
-        </div></>):(<h1>No progress!</h1>)}
+              <br />
+              <div className="handlingfaculty-dashboard-card-container">
+                {courseDataCurrent.map((item, i) => (
+                  <div className="handlingfaculty-dashboard-card" key={i}>
+                    <div className="handlingfaculty-dashboard-card-header">
+                      <span className="grid-item">{item.course_code}</span>
+                      <span className="grid-item">{item.course_name}</span>
+                      <span className="grid-item">{convertToClass1(item.class_id)}</span>
+                      <span className="grid-item">{convertToClass2(item.class_id)}</span>
+                      <span className="grid-item">{convertToClass3(item.class_id)}</span>
+                    </div>
+                    <div className="handlingfaculty-dashboard-card-content">
+                      <p>Hours Completed: {item.completed_hours} / {item.total_hours}</p>
+                      <div className="handlingfaculty-dashboard-progressbar-horizontal">
+                        <div style={{ width: `${value(item.completed_hours, item.total_hours).toFixed(2)}%`, backgroundColor: item.bar_color }} />
+                      </div>
+                      <p>Topics Completed: {courseDataOverall[i].count}/{courseDataOverall[i].total_count}</p>
+                      <div className="handlingfaculty-dashboard-progressbar-horizontal">
+                        <div style={{ width: `${((courseDataOverall[i].count / courseDataOverall[i].total_count) * 100).toFixed(2)}%`, backgroundColor: 'green' }} />
+                      </div>
+                      <div className="handlingfaculty-dashboard-colorcomment">
+                        {renderColorComment(item.bar_color)}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </>
+          ) : (
+            <h1>No progress!</h1>
+          )}
+        </div>
       </div>
-    </div></>
+    </>
   );
 }
 
