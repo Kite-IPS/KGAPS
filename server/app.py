0412baf8e684d7277aa7181b658a178cc3dec17c
NISHANTH1107 <nishanthmn07@gmail.com>
2025-03-20 10:29:29 +0100

    Update database connection string to use new server address

--- server/app.py
+++ server/app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 CORS(app)
 app.secret_key = "helloworld"
 engine = sqlalchemy.create_engine(
-    "postgresql://admin:admin@172.16.30.31/kgaps")
+    "postgresql://admin:admin@172.16.30.20/kgaps")
 conn = engine.connect()
 
 # helper functions
